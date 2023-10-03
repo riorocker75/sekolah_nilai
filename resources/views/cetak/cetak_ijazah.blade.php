d2bf8ebfb767ea946747443668438d83a37b3b38
khairul.umam <khairul.umam@idxpartners.com>
2023-10-04 02:16:44 +0700

    dasd

--- resources/views/cetak/cetak_ijazah.blade.php
+++ resources/views/cetak/cetak_ijazah.blade.php
@@ -99,48 +99,48 @@
     {{-- <body> --}}
 	<div class="wrapper">
 		<div class="container">
-		<div style="margin-top:128px;margin-left:99px;margin-right:99px;">
+		<div style="margin-top:118px;margin-left:99px;margin-right:99px;">
     
             {{-- <div style="position:relative;right:-622px">
                 <p >Swasta</p>
 
             </div> --}}
             <div style="margin-top:40px;">
-                <h5 style='text-align:center;font-weight:400;color:#fff'> KEMENTRIAN PENDIDIKAN</h5>
-                <h5 style='text-align:center;font-weight:400;color:#fff'> KEBUDAYAAN RISET DAN TEKNOLOGI</h5>
-                <h5 style='text-align:center;font-weight:400;color:#fff'> REPUBLIK INDONESIA</h5>
+                <h5 style='text-align:center;font-weight:400;color:transparent'> KEMENTRIAN PENDIDIKAN</h5>
+                <h5 style='text-align:center;font-weight:400;color:transparent'> KEBUDAYAAN RISET DAN TEKNOLOGI</h5>
+                <h5 style='text-align:center;font-weight:400;color:transparent'> REPUBLIK INDONESIA</h5>
                     <br>
                     <br>
                    
-                    <h5 style='text-align:center;font-weight:600;color:#fff'> IJAZAH</h5>
+                    <h5 style='text-align:center;font-weight:600;color:transparent'> IJAZAH</h5>
                     <br>
                     <br>
-                <h5 style='text-align:center;font-weight:400;color:#fff'> SEKOLAH MENENGAH ATAS</h5>
-                <h5 style='text-align:center;font-weight:400;color:#fff'> PEMINATAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h5>
-                 <h5 style='text-align:center;font-weight:400;color:#fff'> TAHUN PELAJARAN {{ date('Y', strtotime('-1 year')) }}/{{date('Y')}}</h5>
+                <h5 style='text-align:center;font-weight:400;color:transparent'> SEKOLAH MENENGAH ATAS</h5>
+                <h5 style='text-align:center;font-weight:400;color:transparent'> PEMINATAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h5>
+                 <h5 style='text-align:center;font-weight:400;color:transparent'> TAHUN PELAJARAN {{ date('Y', strtotime('-1 year')) }}/{{date('Y')}}</h5>
               </div>
         <div style="position:relative;left:140px;width:100%;margin-top:40px;margin-bottom:80px">
             <div style="margin-top:50px;margin-right:300px;">
-                <p style=""><span style="color:#fff">Yand bertanda tangan dibawah ini, Kepala</span> Sekolah Menengah Atas Swasta</p>
+                <p style=""><span style="color:transparent">Yand bertanda tangan dibawah ini, Kepala</span> Sekolah Menengah Atas Swasta</p>
                 <p style="">Ma'had Muhammad Saman Sunggal</p>
-                <p style=""><span style="color:#fff">Nomor Pokok Sekolah Nasional :</span> 10262125</p>
-                <p style=""><span style="color:#fff">Kabupaten / Kota</span> Deli Serdang</p>
-                <p style=""><span style="color:#fff">Provinsi Sumatera Utara</span></p>
-                {{-- <p style=""><span style="color:#fff">menerangkan bahwa</span> </p> --}}
-                <p style=""><span style="color:#fff">Nama</span> <span style='margin-left:190px'>: {{$siswa->nama}}</span></p>
-                <p style=""><span style="color:#fff">Tempat, tanggal lahir</span> <span style='margin-left:85px'>: {{$siswa->tmp_lhr}}, {{format_tanggal(date('Y-m-d',strtotime($siswa->tgl_lhr)))}}</span></p>
-                <p style=""><span style="color:#fff">Nama orang tua / wali</span> <span style='margin-left:75px'>: {{$siswa->ortu}}</span></p>
-                <p style=""><span style="color:#fff">Nomor induk siswa</span> <span style='margin-left:98px'>: {{$siswa->nis}}</span></p>
-                <p style=""><span style="color:#fff">Nomor induk siswa nasional</span> <span style='margin-left:36px'>: {{$siswa->nisn}}</span></p>
+                <p style=""><span style="color:transparent">Nomor Pokok Sekolah Nasional :</span> 10262125</p>
+                <p style=""><span style="color:transparent">Kabupaten / Kota</span> Deli Serdang</p>
+                <p style=""><span style="color:transparent">Provinsi Sumatera Utara</span></p>
+                {{-- <p style=""><span style="color:transparent">menerangkan bahwa</span> </p> --}}
+                <p style=""><span style="color:transparent">Nama</span> <span style='margin-left:190px'>: {{$siswa->nama}}</span></p>
+                <p style=""><span style="color:transparent">Tempat, tanggal lahir</span> <span style='margin-left:85px'>: {{$siswa->tmp_lhr}}, {{format_tanggal(date('Y-m-d',strtotime($siswa->tgl_lhr)))}}</span></p>
+                <p style=""><span style="color:transparent">Nama orang tua / wali</span> <span style='margin-left:75px'>: {{$siswa->ortu}}</span></p>
+                <p style=""><span style="color:transparent">Nomor induk siswa</span> <span style='margin-left:98px'>: {{$siswa->nis}}</span></p>
+                <p style=""><span style="color:transparent">Nomor induk siswa nasional</span> <span style='margin-left:36px'>: {{$siswa->nisn}}</span></p>
 
                 <br>
                 <br>
                
-                <h5 style='text-align:center;font-weight:600;color:#fff'> LULUS</h5>
+                <h5 style='text-align:center;font-weight:600;color:transparent'> LULUS</h5>
                 <br>
                 <br>
-                <p style="color:#fff">Dari sekolah menengah atas setelah memenuhi seluruh kriteria sesuai dengan</p>
-                <p style="color:#fff">Peraturan perundang - undangan yang diumumkan pada tanggal </p>
+                <p style="color:transparent">Dari sekolah menengah atas setelah memenuhi seluruh kriteria sesuai dengan</p>
+                <p style="color:transparent">Peraturan perundang - undangan yang diumumkan pada tanggal </p>
 
 
             </div>
@@ -169,7 +169,7 @@
         </div>
         <div style="margin-left:400px;margin-top:5px">
             <div style="display:inline-block">
-                <span style="margin:35px 15px;color:#fff">NIP.</span>
+                <span style="margin:35px 15px;color:transparent">NIP.</span>
             </div>
         </div>
 
